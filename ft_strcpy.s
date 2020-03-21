3bfb8917006c7feb3238a24053aaaa35769e0207
mrouabeh <mohamed.rouabehi.pro@gmail.com>
2020-03-21 12:09:39 +0100

    Ajout de strdup et correction strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,7 +1,6 @@
 	section .text
 	global _ft_strcpy
 _ft_strcpy:
-	mov		rax, 0
 	mov		rcx, 0
 while:
 	mov bl, byte [rsi + rcx]
@@ -12,6 +11,6 @@ while:
 	jmp while
 end:
 	mov bl, 0
-	mov byte [rdi + rax], bl
+	mov byte [rdi + rcx], bl
 	mov rax, rdi
 	ret
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <mrouabeh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/21 11:25:36 by mrouabeh          #+#    #+#             */
-/*   Updated: 2020/03/21 11:42:09 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2020/03/21 12:07:08 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,40 @@
 
 int main()
 {
+	printf("===== TEST LIBASM =====\n\n");
+
+	printf("=== ft_write === \n");
+	ft_write(1, "Hello\n", 6);
+	ft_write(1, "Hello world\n", 12);
+	ft_write(1, "", 1);
+	ft_write(1, "\n", 1);
+
+	printf("\n=== ft_read === \n");
+	int fd = open("main.c", O_RDONLY);
+	char *buf = NULL;
+	ft_read(fd, buf, 10);
+	printf("%s\n", buf);
+	close(fd);
+
+	printf("\n\n=== ft_strlen === \n");
+	printf("%zu\n", ft_strlen(""));
+	printf("%zu\n", ft_strlen("Hello"));
+	printf("%zu\n", ft_strlen("Hello world\n"));
+
+	printf("\n=== ft_strcmp === \n");
+	printf("%d\n", ft_strcmp("", ""));
+	printf("%d\n", ft_strcmp("Hello", "Hello"));
+	printf("%d\n", ft_strcmp("Hello", "Hel"));
+	printf("%d\n", ft_strcmp("Hel", "Hello"));
+	printf("%d\n", ft_strcmp("Hello", "World"));
+
+	printf("\n=== ft_strcpy ===\n");
+	char dest[100];
+	printf("%s\n", ft_strcpy(dest, "Hello"));
+	printf("%s\n", ft_strcpy(dest, "aaa"));
+	printf("%s\n", ft_strcpy(dest, ""));
+
+	printf("\n=== ft_strdup ===\n");
 	printf("%s\n", ft_strdup("Hello"));
 	printf("%s\n", ft_strdup("world"));
 	printf("%s\n", ft_strdup(""));
